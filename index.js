ce6de743c0dcbfecf639e6de82d65b0908ad16cf
hernan schwindt <hernanschwindt1@gmail.com>
2023-05-10 15:30:58 -0300

    acomodando cosas

---
+++ index.js
@@ -0,0 +1,23 @@
+let stockProductos =[
+    {img: 'img/proyectos/maternidadArgentina.png',lore: 'este es un proyecto realizado con las tecnologias html css js y sass',url:'https://maternidadinfo.netlify.app'},
+    {img:'img/proyectos/tu_lector_de_manga.png',lore:'este es un proyecto realizado con las tecnologias html css bootstrap 5 y sass',url:'https://tu-lector-de-manga.netlify.app'},
+    {img:'', lore:'', url:''}
+];
+
+
+
+
+
+
+
+
+
+
+// <div class="boxEspacio">
+//     <div class="itemsProjects">
+//         <img src="img" alt="">
+//         <p>
+//             lore
+//         </p>
+//     </div>
+// </div>;
